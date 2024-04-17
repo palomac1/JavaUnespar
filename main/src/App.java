407a5313ebc28c1e24ffe751c310d4ecd703d772
Paloma <paloma.dcastroleite@gmail.com>
2024-04-17 14:50:01 -0300

    java

--- main/src/App.java
+++ main/src/App.java
@@ -1,5 +1,17 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // declaracao e inicializacao de variaveis
+    int x = 10;
+    int y = 3;
+// varias operacoes com as variaveis
+    System.out.println("X = " + x);
+    System.out.println("Y = " + y);
+    System.out.println("-X = " + (-x));
+    System.out.println("X/Y = " + (x / y));
+    System.out.println("Resto de X por Y = " + (x % y)); // resulta 1
+    System.out.println("Inteiro de X por Y = " + (int) (x / y)); // resulta 3
+    System.out.println("X + 1 = " + (++x)); // resulta 11
+    
     }
+
 }
